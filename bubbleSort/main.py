a8d27b77573200edcfe0a03af0828bb99c3793ba
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2019-08-01 10:42:02 -0300

    Python

--- bubbleSort/main.py
+++ bubbleSort/main.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 from random import randint
 import timeit
 
-def geraLista(size):
+def geraLista(arr):
 
     array = []
 
-    while len(array) < size:
-        n = randint(1, 1 * size)
+    while len(array) < arr:
+        n = randint(1, 1 * arr)
         if n not in array: array.append(n)
-
     return array
 
 operacoes = []
+arr = [100, 200, 500, 1000]
+aux = []
 
 def bubbleSort(array):
     ciclos = 0
@@ -23,31 +24,27 @@ def bubbleSort(array):
             if array[x] > array[x + 1]:
                 array[x], array[x + 1] = array[x + 1], array[x]
                 ciclos += 1
-
     operacoes.append(ciclos)
 
-def showGraph(x, y, xl = "Inputs", yl = "Outputs", imgName = "img"):
+
+def showGraph(x, y, imgName = "img", xl = "Inputs", yl = "Outputs"):
     fig = plt.figure(figsize=(12, 16))
-    ax = fig.add_subplot(111)
-    ax.plot(x, y, label = "Melhor Tempo")
-    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.plot(x, y, label = "Melhor Tempo")
+    plt.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
     plt.savefig(imgName)
 
-
-array = [10, 20, 50, 100]
-arrBubble = []
-arrTime = []
-
-for i in range(len(array)):
+for i in arr:
   list = geraLista(i)
-  arrBubble.append(timeit.timeit("bubbleSort({})".format(geraLista(array[i])), setup="from __main__ import geraLista,bubbleSort", number = 1))
-  print(i)
+  aux.append(geraLista(i))
+  print("Pos = ", i)
 
-showGraph(array, arrTime, imgName = "Tempo")
-showGraph(array, operacoes, imgName = "Iterações no laço")
+time =[]
+for i in range(len(aux)):
+    time.append(timeit.timeit("bubbleSort({})".format(aux[i]),setup="from __main__ import bubbleSort",number=1))
 
-#for i in range(len(array)):
+print("Tempo:", time)
 
-  #arrTime.append(bubbleSort(geraLista(array[i])))
+showGraph(arr, time, 'Tempo', 'Numbers', 'Time')
+showGraph(arr, operacoes, 'Iterações no laço')
