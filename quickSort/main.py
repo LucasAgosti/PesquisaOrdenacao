7d6c352218f78656dcf402fa056a711653cb62cd
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2019-08-13 22:59:25 -0300

    Python

--- quickSort/main.py
+++ quickSort/main.py
@@ -12,13 +12,6 @@ def geraLista(arr):
       if n not in array: array.append(n)
     return array
 
-def listaDec(arr):
-    list = []
-    while arr > 0:
-        list.append(arr)
-        arr -= 1
-    return list
-
 def quickSort(array, esquerda, direita):
     if esquerda < direita:
         pivo = randint(esquerda, direita)
@@ -47,39 +40,21 @@ def partition(array, esquerda, direita):
 
     return pivo_index + 1
 
-def showGraph(x, y, r, eixo, xl = "Números de elementos", yl = "Tempo", imgName = "insertionSort"):
+def showGraph(x, y, xl = "Números de elementos", yl = "Saidas", imgName = "quickSort"):
     fig = plt.figure(figsize=(12, 16))
-    plt.plot(x, y, label="Vetor aleatório {} ".format(eixo))
-    plt.plot(x, r, label="Vetor decrescente {} ".format(eixo))
+    plt.plot(x, y, label="Vetor aleatório {} ")
     plt.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
     plt.savefig(imgName)
 
-operacoes = []
-arr = [10000, 20000, 50000, 100000]
+arr = [10000, 10000, 500000, 2000000]
 aux = []
-auxDec = []
 
-for i in arr:
-  list = geraLista(i)
-  aux.append(geraLista(i))
-  auxDec.append(listaDec(i))
-
-time =[]
-timeDec = []
-time2 =[]
-timeDec2 = []
-
-for i in range(len(aux)):
-    time.append(timeit.timeit("insertionSort({})".format(aux[i]),setup="from __main__ import insertionSort",number=1))
-    timeDec.append(timeit.timeit("insertionSort({})".format(auxDec[i]),setup="from __main__ import insertionSort",number=1))
+for i in range(len(arr)):
+    arrayFinal = geraLista(i)
+    aux.append(timeit.timeit("quickSort({}, {}, {})".format(arrayFinal, 0, i - 1),setup="from __main__ import quickSort",number=1))
+  
     print(i)
 
-showGraph(arr, time, timeDec, eixo = "Tempo", imgName = 'Tempo')
-
-for i in range(len(aux)):
-  time2.append(insertionSort(aux[i]))
-  timeDec2.append(insertionSort(auxDec[i]))
-
-showGraph(arr, time2, timeDec2, eixo = "Trocas", yl = 'Iterações', imgName = 'N de iterações no laço')
+showGraph(arr, aux)
